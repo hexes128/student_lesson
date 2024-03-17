91e218ad79a958066ffa7e76fbd08ec03aaa6a77
hexes128 <50615028+hexes128@users.noreply.github.com>
2024-03-17 16:18:41 +0800

    add lesson description

--- lib/view_model/lesson.dart
+++ lib/view_model/lesson.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 class Lesson {
   String lessonName;
   String weekDay;
+  String lessonDescription;
   TimeOfDay lessonStartTime;
   TimeOfDay lessonEndTime;
 
-  Lesson({required this.lessonName, required this.weekDay, required this.lessonStartTime, required this.lessonEndTime});
+  Lesson({required this.lessonName, required this.weekDay, required this.lessonStartTime, required this.lessonEndTime,required this.lessonDescription});
 }
