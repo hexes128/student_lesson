8a7fcf2e45dd51934b540dc9d813fd5be01b8c66
hexes128 <50615028+hexes128@users.noreply.github.com>
2024-03-17 16:18:54 +0800

    add go router pakage

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:student_lesson/provider/lecturer_lesson_provider.dart';
+import 'package:student_lesson/screen/lesson_information_screen.dart';
+import 'package:student_lesson/view_model/lecturer.dart';
+import 'package:student_lesson/view_model/lesson.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,58 +17,116 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(),
+      routerConfig: GoRouter(
+        routes: [
+          GoRoute(path: '/', builder: (context, state) => MyHomePage(), routes: [
+            GoRoute(
+              path: 'lesson_information_screen',
+              name: 'lesson_information_screen',
+              builder: (context, state) => LessonInformationScreen(
+                lesson: state.extra as Lesson,
+              ),
+            ),
+          ]),
+        ],
+      ),
     );
   }
 }
 
 class MyHomePage extends HookConsumerWidget {
-  MyHomePage({super.key});
+  final StateNotifierProvider<LecturerStateNotifier, AsyncValue<List<Lecturer>>> lecturerProvider;
 
-  final List<Lecturer> lecturerList = [
-    Lecturer(name: "Albert Flores",jobTitle: "Demonstrator",profilePicture: "assets/albert_flores.png"),
-    Lecturer(name: "Floyd Miles",jobTitle: "Lecturer",profilePicture: "assets/floyd_miles.jpg"),
-    Lecturer(name: "Savannah Nguyen",jobTitle: "Senior Lecturer",profilePicture: "assets/jenny_wilson.jpg"),
-    Lecturer(name: "Jenny Wilson",jobTitle: "Professor",profilePicture: "assets/neil_leveridge.jpg"),
-    Lecturer(name: "Neil Leveridge",jobTitle: "Professor",profilePicture: "assets/savannah_nguyen.jpg"),
-  ];
+  MyHomePage({super.key}) : lecturerProvider = StateNotifierProvider<LecturerStateNotifier, AsyncValue<List<Lecturer>>>((ref) => LecturerStateNotifier());
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(body: ListView.builder(itemCount: lecturerList.length, itemBuilder: (context, index) {
+    final lessonIsExpanded = useState(false); //check if user expands the lesson list from lecturer
 
-      return ExpansionTile(title: ListTile(leading: CircleAvatar(
-        backgroundImage: AssetImage(lecturerList[index].profilePicture), 
-      ) ,));
-    }));
+    return Scaffold(
+        appBar: AppBar(
+          leading: const Icon(Icons.arrow_back_outlined),
+          title: const Text("講師清單"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: ref.watch(lecturerProvider).when(data: (lecturerList) {
+            return ListView.builder(
+                itemCount: lecturerList.length,
+                itemBuilder: (context, index) {
+                  final lecturer = lecturerList[index];
+                  return Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                    child: ExpansionTile(
+                      trailing: lessonIsExpanded.value ? const Icon(Icons.remove) : const Icon(Icons.add),
+                      shape: const Border(),
+                      title: ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: AssetImage(lecturer.profilePicture),
+                        ),
+                        title: Text(
+                          lecturer.jobTitle,
+                          style: const TextStyle(color: Colors.grey),
+                        ),
+                        subtitle: Text(
+                          lecturer.name,
+                          style: const TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      children: [
+                        const Divider(
+                          color: Colors.grey,
+                          thickness: 1.0,
+                          height: 1.0,
+                        ),
+                        ...lecturer.lessonList.map((e) => LessonListTile(lesson: e))
+                      ],
+                      onExpansionChanged: (expanded) {
+                        lessonIsExpanded.value = expanded;
+                      },
+                    ),
+                  );
+                });
+          }, error: (e, s) {
+            return const Text("錯誤");
+          }, loading: () {
+            return const Center(child: CircularProgressIndicator());
+          }),
+        ));
   }
 }
 
-class Lecturer {
-  String name;
-  String jobTitle;
-  String profilePicture;
+class LessonListTile extends StatelessWidget {
+  final DefaultMaterialLocalizations timeOfDayFormatter;
 
-  Lecturer({required this.name, required this.jobTitle, required this.profilePicture});
+  final Lesson lesson;
+
+  const LessonListTile({super.key, required this.lesson, this.timeOfDayFormatter = const DefaultMaterialLocalizations()});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: const Icon(Icons.calendar_month),
+      title: Text(lesson.lessonName),
+      subtitle: Text(
+        "${lesson.weekDay}, ${timeOfDayFormatter.formatTimeOfDay(lesson.lessonStartTime, alwaysUse24HourFormat: true)}~${timeOfDayFormatter.formatTimeOfDay(lesson.lessonEndTime, alwaysUse24HourFormat: true)}",
+      ),
+      trailing: IconButton(
+        icon: const Icon(Icons.navigate_next),
+        onPressed: () {
+          context.pushNamed("lesson_information_screen", extra: lesson);
+        },
+      ),
+      onTap: () {
+        context.pushNamed("lesson_information_screen", extra: lesson);
+      },
+    );
+  }
 }
